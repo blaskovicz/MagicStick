7a70de86e29eaae33c60a11b11e2f9faefb4c0c8
Ben Heiskell <ben.heiskell@gmail.com>
2015-04-12 20:56:51 -0400

    fix(db): alter migrations to work with postgres

--- db/migrations/001_roles.rb
+++ db/migrations/001_roles.rb
@@ -27,7 +27,6 @@ Sequel.migration do
       foreign_key :user_id, :users, :on_delete => :cascade, :on_update => :cascade
       foreign_key :role_id, :roles, :on_delete => :cascade, :on_update => :cascade
       primary_key [:user_id, :role_id]
-      index [:user_id, :role_id], :unique => true
     end
     [
       {:name => "admin", :description => "site admin"},
--- db/migrations/002_matches.rb
+++ db/migrations/002_matches.rb
@@ -12,7 +12,9 @@ Sequel.migration do
       FalseClass :invite_only, :null => false, :default => false
       TrueClass :allow_auto_join, :null => false, :default => true
       foreign_key :owner_id, :users, :null => false, :on_delete => :cascade, :on_update => :cascade
-      index [:owner_id, :name], :unique => true
+    end
+    alter_table(:seasons) do
+      add_index [:owner_id, :name], :unique => true
     end
     create_table?(:season_match_groups) do
       primary_key :id
@@ -20,7 +22,9 @@ Sequel.migration do
       DateTime :updated_at
       String :name, :null => false, :size => 64
       foreign_key :season_id, :seasons, :null => false, :on_delete => :cascade, :on_update => :cascade
-      index [:name, :season_id], :unique => true
+    end
+    alter_table(:season_match_groups) do
+      add_index [:name, :season_id], :unique => true
     end
     create_table?(:users_seasons) do
       primary_key :id
@@ -28,15 +32,17 @@ Sequel.migration do
       DateTime :updated_at
       foreign_key :user_id, :users, :on_delete => :cascade, :on_update => :cascade
       foreign_key :season_id, :seasons, :on_delete => :cascade, :on_update => :cascade
-      index [:user_id, :season_id], :unique => true
-    end    
+    end
+    alter_table(:users_seasons) do
+      add_index [:user_id, :season_id], :unique => true
+    end
     create_table?(:matches) do
       primary_key :id
       DateTime :created_at
       DateTime :updated_at
       DateTime :scheduled_for, :null => false
       DateTime :completed, :null => true
-      foreign_key :season_match_group_id, :season_match_group, :null => false, :on_delete => :cascade, :on_update => :cascade
+      foreign_key :season_match_group_id, :season_match_groups, :null => false, :on_delete => :cascade, :on_update => :cascade
       String :description, :null => true, :size => 4000
     end
     create_table?(:users_seasons_matches) do
@@ -46,7 +52,6 @@ Sequel.migration do
       foreign_key :user_season_id, :users_seasons, :on_delete => :cascade, :on_update => :cascade
       foreign_key :match_id, :matches, :on_delete => :cascade, :on_update => :cascade
       primary_key [:user_season_id, :match_id]
-      index [:user_season_id, :match_id], :unique => true
     end
     create_table?(:matches_comments) do
       primary_key :id
