2e38df61fa33de752e24f89b40ba484ab8054984
Ben Heiskell <ben.heiskell@gmail.com>
2015-04-18 19:20:47 -0400

    refactor: switch to defaulted case assignment for DATABASE_URL

--- db/init.rb
+++ db/init.rb
@@ -2,15 +2,13 @@ require 'sequel'
 Sequel::Model.plugin :json_serializer
 Sequel::Model.plugin :timestamps, :update_on_create => true
 
-$db_url = ENV["DATABASE_URL"]
-
-case ENV["RACK_ENV"]
+$db_url = ENV["DATABASE_URL"] || case ENV["RACK_ENV"]
 when "production"
-  $db_url ||= "postgres://localhost/magicstick"
+  "postgres://localhost/magicstick"
 when "test"
-  $db_url ||= "sqlite://test.db"
+  "sqlite://test.db"
 else
-  $db_url ||= "sqlite://magicstick.db"
+  "sqlite://magicstick.db"
 end
 
 $DB = Sequel.connect($db_url)
