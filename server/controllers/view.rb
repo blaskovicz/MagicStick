b257d7e32ed4f2c1f4281e992911bf24a49bf040
Zach Auclair <zach101@gmail.com>
2017-03-14 22:15:44 -0400

    feat(runtime/env): allow passthrough of special env vars at runtime
    SINATRA_PUBLIC_ENV_ prefixed vars will have the prefix stripped and
    the value passed through as a MagicStick js global.

--- server/controllers/view.rb
+++ server/controllers/view.rb
@@ -1,6 +1,15 @@
 class ViewController < ApplicationController
+  PREFIX = 'SINATRA_PUBLIC_ENV_'.freeze
   get '/' do
-    content_type 'text/html'
-    send_file File.expand_path('index.html', settings.public_folder)
+    # eg: SINATRA_PUBLIC_ENV_RAYGUN_API_KEY=bar would
+    # be set to RAYGUN_API_KEY=bar in @env
+    @env = ENV.each_with_object({}) do |field, o|
+      next unless field.first.start_with? PREFIX
+      o[field.first.sub(PREFIX, '')] = field.last
+    end
+    @env[:MAGIC_STICK_VERSION] = VERSION
+    @env[:RACK_ENV] = ENV['RACK_ENV']
+    content_type :html
+    erb :index
   end
 end
