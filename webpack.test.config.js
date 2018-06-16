c926a613dccfe33c19751632afb3723f899c4b6d
Zach Auclair <zach101@gmail.com>
2018-06-16 00:15:17 -0400

    fix(webpack): ~ just javascript things ~

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,13 +1,86 @@
 const path = require("path");
-const webpackConfig = require("./webpack.config");
-
-Object.assign(webpackConfig, {
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const webpackEnv = process.env.WEBPACK_ENV || "development";
+module.exports = {
   entry: path.resolve(__dirname, "frontend", "specs.js"),
+  mode: webpackEnv,
   devtool: "cheap-module-inline-source-map",
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "specs.js"
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.css$/,
+          chunks: "all",
+          enforce: true
+        }
+      }
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    // new MiniCssExtractPlugin({
+    //   filename: "[name].css"
+    // }),
+    new CleanWebpackPlugin(["public"], { exclude: ["img"], verbose: false })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader"
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000
+        }
+      }
+    ]
   }
-});
-
-module.exports = webpackConfig;
+};
